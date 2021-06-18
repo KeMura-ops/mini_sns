d6f43017b0ac4c3910595f3c6077ef705cd23020
KeMura-ops <kenti432@yahoo.co.jp>
2021-06-18 10:02:42 +0900

    パスワード入力を不要にする記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,11 +14,11 @@ class User < ApplicationRecord
       nickname: auth.info.name,
          email: auth.info.email
     )
-    # userが登録済みであるか判断
+    # userが登録済みの場合はそのままログインの処理へ行くので、ここでsnsのuser_idを更新しておく
     if user.persisted?
       sns.user = user
       sns.save
     end
-    user
+    { user: user, sns: sns }
   end
 end
